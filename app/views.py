3651b6ec2c67fdd48275b1fc065fc59ce78bdd6c
gaoqinghong <1134639599@qq.com>
2020-04-26 14:17:12 +0800

    假如app版本index页面

--- app/views.py
+++ app/views.py
@@ -23,6 +23,23 @@ def home():
 
     return resp
 
+@app.route('/m')
+def home_m():
+    print (session.get('session_id'))
+    if session.get('session_id') is None:
+        session['session_id'] = '1234567890'
+    docs = []
+    num = 0
+    for o in client.blog.doc.find().limit(10):
+        docs.append({'title':o['title'],'url':o['url'], 'subtitle': o['text'].strip()[0:80]})
+
+    # return render_template('index.html', docs=docs)
+
+    resp = make_response(render_template('app_index.html', docs=docs))
+    resp.set_cookie('session_id', 'the username')
+
+    return resp
+
 @app.route('/template')
 def template():
     return render_template('home.html')
